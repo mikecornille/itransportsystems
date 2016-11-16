a5334b95aa8ffab7717498dd5823413e70c1fe23
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2016-11-16 09:43:19 -0600

    different phone validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:cms_users',
-            'cell' => 'required|regex:/(01)[0-9]{9}/|unique:cms_users',
+            'cell' => 'required|digits:10|unique:cms_users',
             'password' => 'required|min:6|confirmed',
             
         ]);
