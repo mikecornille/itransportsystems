8e5d3cef8d2aa1ff7b2635344a63fd8f1c7eaef7
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2016-11-17 13:31:50 -0600

    validation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 
 <form role="form" class="form-horizontal" method="POST" action="/new">
         
