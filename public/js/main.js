6a150eed98dd7eeed81a67afd9833cd93d70ae67
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2016-11-17 14:51:44 -0600

    success

--- public/js/main.js
+++ public/js/main.js
@@ -67,29 +67,29 @@ $(document).ready(function() {
 
 //MAKE EACH COLUMN SEARCHABLE AND SORTABLE
 
-// $(document).ready(function() {
-//     // Setup - add a text input to each footer cell
-//     $('#mainTable tfoot th').each( function () {
-//         var title = $(this).text();
-//         $(this).html( '<input type="text" placeholder="'+title+'" />' );
-//     } );
+$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('#mainTable tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="'+title+'" />' );
+    } );
  
-//     // DataTable
-//     var table = $('#mainTable').DataTable();
+    // DataTable
+    var table = $('#mainTable').DataTable();
  
-//     // Apply the search
-//     table.columns().every( function () {
-//         var that = this;
+    // Apply the search
+    table.columns().every( function () {
+        var that = this;
  
-//         $( 'input', this.footer() ).on( 'keyup change', function () {
-//             if ( that.search() !== this.value ) {
-//                 that
-//                     .search( this.value )
-//                     .draw();
-//             }
-//         } );
-//     } );
-// } );
+        $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+                that
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
+} );
 
 
 
