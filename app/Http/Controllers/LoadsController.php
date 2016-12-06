699efb48aac886ca15a6cb4eecf6e85a10442216
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2016-12-06 15:11:39 -0600

    added more

--- app/Http/Controllers/LoadsController.php
+++ app/Http/Controllers/LoadsController.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Mail;
 
 class LoadsController extends Controller
 {
-	use helpers\Mailer;
+	//use helpers\Mailer;
     
 	//Load the datatable
     public function index()
