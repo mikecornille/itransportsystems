03cbf2573d71ac678a4103819f2b06d2eac087e7
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2017-01-10 11:03:03 -0600

    updated the BOL

--- resources/views/myStats.blade.php
+++ resources/views/myStats.blade.php
@@ -13,8 +13,19 @@
 <h3 class="text-left">{{ $invoices }} Invoices</h3>
 </div>
 
-@foreach ($unsigned as $unsign)
-    <p>This is user {{ $unsign->rate_con_creator . ' ' . $unsign->id }}</p>
-@endforeach
+
+<div style="margin-left: 300px;">
+<h3 class="text-left">Unsigned Rate Confirmations</h3>
+	<ul style="list-style-type: none;">
+	@foreach ($unsigned as $unsign)
+	    <li class="text-left"><u>PRO # {{ $unsign->id }} is unsigned.</u></li>
+	    <li class="text-left">Short Status Note: {{ $unsign->quick_status_notes }}</li>
+	    <li class="text-left">Dispatcher Phone: {{ $unsign->carrier_contact }} at {{ $unsign->carrier_phone }}</li>
+	    <li class="text-left">Dispatcher Email: {{ $unsign->carrier_email }}</li>
+	@endforeach
+	</ul>
+</div>
+
+
 
 @endsection
