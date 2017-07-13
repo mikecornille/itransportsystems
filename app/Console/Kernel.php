ca113ae02a7b252b2e64ac54d26ed84ade3d3ebc
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2017-07-13 10:02:20 -0500

    weekly report

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
         $schedule->command('backup:clean')->daily()->at('01:00');
         $schedule->command('backup:run')->daily()->at('02:00');
+        $schedule->command('generate')->weekly()->thursdays()->at('10:10');
         
     }
 
--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 
+use App\Http\Requests;
+use Illuminate\Support\Facades\Mail;
+use App\Item;
+use App\User;
+use App\Load;
+use App\Loadlist;
+
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -16,3 +23,46 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+Artisan::command('generate', function () {
+	$type = 'csv';
+	$start_date = '06/01/2017';
+		 $end_date = '06/04/2017';
+    $loads = Load::select('billed_date', 'approved_carrier_invoice', 'its_group', 'id', 'pick_city', 'pick_state', 'delivery_city', 'delivery_state', 'customer_name', 'amount_due', 'carrier_name', 'carrier_rate')->whereBetween('billed_date', [$start_date, $end_date])->orderBy('id', 'asc')->get();
+
+		$data = \Excel::create('Profit_Report_' . $start_date . '_to_' . $end_date, function($excel) use ($loads) {
+			$excel->sheet('mySheet', function($sheet) use ($loads)
+	        {
+				$sheet->fromArray($loads);
+	        });
+		});
+		        if ($loads instanceof Illuminate\Support\Collection) {
+
+        			$keys = array_keys($loads->first()->toArray());
+        	}	else {
+        		$keys = array_keys($loads[0]);
+        	}
+		$savePath = storage_path('csv/' . 'itransys_report.csv');
+		$fp = fopen($savePath, 'w');
+		fputcsv($fp,$keys);
+		foreach ($loads as $key => $value) {
+	        fputcsv($fp, $value->toArray());
+		}
+
+        fclose($fp);
+       $info = ["foo" => "bar", "bar" => "foo"];
+
+           Mail::send(['html'=>'email.body'], $info, function($message) use ($info, $savePath){
+
+        
+
+       	$recipients = ['mikec@itransys.com'];
+
+        $message->to($recipients)->subject('Truckstop Posted')
+			->from($recipients[0], $recipients[0])
+			->replyTo($recipients[0], $recipients[0])
+			->sender($recipients[0], $recipients[0]);
+
+        	$message->attach($savePath);
+        });
+
+})->describe('Generate report');
