25efb64c0c72115b8dd98df6c93a7f62b59685da
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2017-07-24 08:37:17 -0500

    rearrange email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,10 +29,10 @@ class Kernel extends ConsoleKernel
         $schedule->command('backup:clean')->daily()->at('01:00');
         $schedule->command('backup:run')->daily()->at('02:00');
         $schedule->command('weeklyProfitReport')->weekly()->fridays()->at('12:00')->timezone('America/Chicago');
-        // $schedule->command('dailyCarriersSetUp')->daily()->at('14:45')->timezone('America/Chicago');
-        // $schedule->command('screamerCheck')->daily()->at('11:15')->timezone('America/Chicago');
-        // $schedule->command('brokerCallEmail')->twiceDaily(10, 14)->timezone('America/Chicago');
-        // $schedule->command('followUpScreamerEmail')->daily()->at('12:30')->timezone('America/Chicago');
+        $schedule->command('dailyCarriersSetUp')->daily()->at('14:45')->timezone('America/Chicago');
+        $schedule->command('screamerCheck')->daily()->at('11:15')->timezone('America/Chicago');
+        $schedule->command('brokerCallEmail')->twiceDaily(10, 14)->timezone('America/Chicago');
+        $schedule->command('followUpScreamerEmail')->daily()->at('12:30')->timezone('America/Chicago');
 
         
     }
--- resources/views/email/screamerCheck.blade.php
+++ resources/views/email/screamerCheck.blade.php
@@ -1,5 +1,5 @@
-<p>Below are the screaming loads on the list for today's date, let Matt King know if you have had any correspondence with the customer, shipper, or receiver regarding the shipments you created.  If you have not, King will use his soothing voice to strategically contact the shipper/receiver for a true heat check.  So please either inform King you made all the necessary calls to get an accruate heat check or give him the go ahead to investigate.</p>
-
 @for ($i = 0; $i < count($info); $i++)
     <p>Lane: {{ $info[$i]['pick_city'] }}, {{ $info[$i]['pick_state'] }} to {{ $info[$i]['delivery_city'] }}, {{ $info[$i]['delivery_state'] }} created by {{ $info[$i]['created_by'] }} for {{ $info[$i]['customer'] }} - urgency - {{ $info[$i]['urgency'] }} </p>
 @endfor
+
+<p>Let Matt King know if you have had any correspondence with the customer, shipper, or receiver regarding the shipments you created.  If you have not, King will use his soothing voice to strategically contact the shipper/receiver for a true heat check.  So please either inform King you made all the necessary calls to get an accruate heat check or give him the go ahead to investigate.</p>
