5f410368b7d9b7b3b3c8da2a507ebc87e6cf8241
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2018-01-31 15:50:08 -0600

    user gets email when text is received

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -119,7 +119,7 @@ class TextController extends Controller
         Mail::send(['html'=>'email.textNotification'], $load, function($message) use ($load){
             
             
-            $message->to(\Auth::user()->email)
+            $message->to($load['load']['rate_con_creator'])
 
             ->subject('You Received a Text Message!');
           
