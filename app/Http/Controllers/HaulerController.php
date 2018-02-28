ac692dec7c040bf589c9c28a39ccefbf1198e102
Michael Cornille <michaelcornille@Michaels-MBP.local.tld>
2018-02-28 08:52:16 -0600

    added back ability to create new carrier from scratch

--- app/Http/Controllers/HaulerController.php
+++ app/Http/Controllers/HaulerController.php
@@ -37,7 +37,7 @@ class HaulerController extends Controller
 
         $store = New Carrier();
 
-        $store->mc_number = 0;
+        
         $store->company = $find_record[0]->LEGAL_NAME;
         $store->dot_number = $find_record[0]->DOT_NUMBER;
         $store->address = $find_record[0]->PHY_STREET;
