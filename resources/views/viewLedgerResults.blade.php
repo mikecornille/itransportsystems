60d9175b858591741a563d8a6d15d78c066d800a
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2019-01-18 14:45:26 -0600

    added featured to ledger view

--- resources/views/viewLedgerResults.blade.php
+++ resources/views/viewLedgerResults.blade.php
@@ -2,18 +2,38 @@
 
 @section('content')
 
+<div class="container">
 
-<h3>Starting Balance {{ $startingBalance }}</h3>
+	@PHP setlocale(LC_MONETARY, 'en_US.UTF-8'); @ENDPHP 
 
-<h3>Ending Balance {{ $endingBalance }}</h3>
+<h3>Starting Balance : {{ money_format('%.2n', $startingBalance) }}</h3>
 
+<h3>Ending Balance : {{ money_format('%.2n', $endingBalance) }}</h3>
+
+<table class="table">
+	<thead>
+		<tr>
+			<th>Date</th>
+			<th>Name</th>
+			<th>Reference Number</th>
+			<th>Deposit Amount</th>
+			<th>Payment Amount</th>
+			<th>Running Total</th>
+		</tr>	
+	</thead>
+	<tbody>
 
 @foreach($data as $d)
-
-{{ $d->account_name  . ' ' . $d->running_total }}
-<br>
-
+	<tr>
+		<td>{{ $d->date }}</td>
+		<td>{{ $d->account_name }}</td>
+		<td>{{ $d->reference_number }}</td>
+		<td class="text-primary">{{ $d->deposit_amount ? '$' . $d->deposit_amount : '' }}</td>
+		<td class="text-danger">{{ $d->payment_amount ? '$' . $d->payment_amount : '' }}</td>
+		<td>{{ money_format('%.2n', $d->running_total) }}</td>
+	</tr>
 @endforeach
-
-
+	</tbody>
+</table>
+</div>
 @endsection
