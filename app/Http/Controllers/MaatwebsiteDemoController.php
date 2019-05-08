0a0a6d064b2dc82a7c51a503e66c25be864e76d6
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2019-05-08 13:03:52 -0500

    changed ach upload file

--- app/Http/Controllers/MaatwebsiteDemoController.php
+++ app/Http/Controllers/MaatwebsiteDemoController.php
@@ -487,7 +487,7 @@ class MaatwebsiteDemoController extends Controller
   //   			return $carrier_invoices;
 		// 	});
 
-		  $carrier_invoices = Load::select('id', 'account_name', 'carrier_id', 'account_number', 'vendor_invoice_number', 'routing_number', 'carrier_rate', 'carrier_name' )
+		  $carrier_invoices = Load::select('account_name', 'carrier_id', 'account_number', 'vendor_invoice_number', 'routing_number', 'carrier_rate', 'carrier_name' )
 		 ->where('payment_method', "ACH")->where('carrierPayStatus', "APPRVD")
 		 ->whereRaw("STR_TO_DATE(`vendor_invoice_date`, '%m/%d/%Y') >= STR_TO_DATE('{$start_date}', '%m/%d/%Y')")
 		 ->whereRaw("STR_TO_DATE(`vendor_invoice_date`, '%m/%d/%Y') <= STR_TO_DATE('{$end_date}', '%m/%d/%Y')")
